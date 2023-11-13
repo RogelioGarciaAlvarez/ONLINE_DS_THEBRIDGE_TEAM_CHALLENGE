9c19e96fd92eadae9a8755324554691252086ba6
Rogelio Garcia <147382528+RogelioGarciaAlvarez@users.noreply.github.com>
2023-11-13 08:25:31 +0100

    avance 12-11

--- funciones.py
+++ funciones.py
@@ -3,8 +3,8 @@ import numpy as np
 def posicionar_aleatorio(tablero_barcos, tamano_barco, limite_filas =9 , limite_columnas = 9): #Ver como hacer el 9 de manera dinamica
     
     #Usamos un bucle while para generar la posicion y orientacion del barco aleatorio hasta que se pueda colocar en el tablero
-    fuera_tablero = True
-    while fuera_tablero == True:
+    fuera_tablero = False
+    while fuera_tablero == False:
         #Generamos dos números enteros aleatorios entre 0 y 9 que nos dará el origen para colocar el barco
         origen_fila = np.random.randint(10)
         origen_columna = np.random.randint(10)
@@ -17,7 +17,6 @@ def posicionar_aleatorio(tablero_barcos, tamano_barco, limite_filas =9 , limite_
         #print(f"El barco tiene como origen la fila {origen_fila} y la columna {origen_columna}") 
         
         #Comprobamos si se puede posicionar el barco suponiendo que inicialmente no esta fuera del tablero (fuera_tablero == False)
-        fuera_tablero = False
         if orientacion == "Norte":
             if origen_fila - (tamano_barco-1) < 0:
                 fuera_tablero = True
@@ -36,10 +35,9 @@ def posicionar_aleatorio(tablero_barcos, tamano_barco, limite_filas =9 , limite_
 
         #Si no hay coordenadas fuera del tablero, se posiciona el barco siempre y cuando no haya otro barco
         if fuera_tablero == False:
-
+            hay_barco = False #Inicializamos suponiendo que no hay barco
             if orientacion == "Norte":
                 #Se comprueba si ya hay un barco
-                hay_barco = False #Inicializamos suponiendo que no hay barco
                 barco_aleatorio = tablero_barcos[origen_fila-(tamano_barco-1):origen_fila+1, origen_columna:origen_columna+1]
                 hay_barco = np.any(np.isin(barco_aleatorio, "O")) or np.any(np.isin(barco_aleatorio, "X")) #Comprueba si hay algun True en la lista que genera .isin
                 
@@ -48,7 +46,6 @@ def posicionar_aleatorio(tablero_barcos, tamano_barco, limite_filas =9 , limite_
 
             elif orientacion == "Sur":
                 #Se comprueba si ya hay un barco
-                hay_barco = False #Inicializamos suponiendo que no hay barco
                 barco_aleatorio = tablero_barcos[origen_fila:origen_fila+tamano_barco, origen_columna:origen_columna+1]
                 hay_barco = np.any(np.isin(barco_aleatorio, "O")) or np.any(np.isin(barco_aleatorio, "X"))
 
@@ -57,16 +54,14 @@ def posicionar_aleatorio(tablero_barcos, tamano_barco, limite_filas =9 , limite_
             
             elif orientacion == "Este":
                 #Se comprueba si ya hay un barco
-                hay_barco = False #Inicializamos suponiendo que no hay barco
                 barco_aleatorio = tablero_barcos[origen_fila:origen_fila+1, origen_columna:origen_columna+tamano_barco]
                 hay_barco = np.any(np.isin(barco_aleatorio, "O")) or np.any(np.isin(barco_aleatorio, "X"))
 
                 if hay_barco == False:
                     tablero_barcos[origen_fila:origen_fila+1, origen_columna:origen_columna+tamano_barco] = "O"
     
-            elif orientacion == "Oeste": #Oeste
+            elif orientacion == "Oeste":
                 #Se comprueba si ya hay un barco
-                hay_barco = False #Inicializamos suponiendo que no hay barco
                 barco_aleatorio = tablero_barcos[origen_fila:origen_fila+1, origen_columna-(tamano_barco-1):origen_columna+1]
                 hay_barco = np.any(np.isin(barco_aleatorio, "O")) or np.any(np.isin(barco_aleatorio, "X"))
 
--- main.py
+++ main.py
@@ -9,12 +9,9 @@ tablero_maquina = clases.Tablero(variables.DIMENSION_FILA, variables.DIMENSION_C
 
 
 funciones.posicionar_aleatorio(tablero_usuario.tablero_barcos, 4)
-funciones.posicionar_aleatorio(tablero_usuario.tablero_barcos, 4)
-funciones.posicionar_aleatorio(tablero_usuario.tablero_barcos, 4)
 
 print(tablero_usuario.tablero_barcos)  
 print("Fin posicionamiento")
 
-#HE VISTO QUE AUNQUE LO PONGO 3 VECES, A VECES PONE 3 BARCOS Y A VECES 2
-#CREO QUE ESTO PASA PORQUE EN ALGUNA ORIENTACION NO ESTA MIRANDO BIEN SI SE SALE DE LOS LIMITES Y ESTA COLOCANDO UN BARCO FUERA.
+#A VECES PONE 3 BARCOS, A VECES 2 Y A VECES 1
 #EL MIERCOLES LO REVISAMOS :)
