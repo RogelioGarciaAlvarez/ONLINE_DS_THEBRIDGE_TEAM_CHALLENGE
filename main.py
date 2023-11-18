b1eebfe3f5302933e623e2dd52eff990df80666d
Rogelio Garcia <147382528+RogelioGarciaAlvarez@users.noreply.github.com>
2023-11-18 14:43:42 +0100

    avances 18-11

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import random
 import sys
 import variables
 
+#Instrucciones 
+print("Bienvenidos al juego de hundir la flota 'El Victory'")
+print("Las instrucciones son muy sencillas:")
+print("-Hay dos jugadores: tú y la máquina. Empiezas tú")
+print("-En cada tablero hay 4 barcos de 1 posición de eslora, 3 barcos de 2 posiciones de eslora, 2 barcos de 3 posiciones de eslora y 1 barco de 4 posiciones de eslora")
+print("-El juego te pedirá al principio que selecciones un nivel de dificultad")
+print("-Cuando sea tu turno tendrás que introducir las coordenadas de fila, primero, y columna, después, separadas por coma, cuyos valores deben estar entre 0 y 9")
+print("-El orden de las filas y columnas es de arriba a abajo y de izquierda a derecha")
+print("-Cada vez que aciertes te vuelve a tocar disparar. Si fallas, es el turno de la máquina que te disparará aleatoriamente")
+print("-El juego finalizará cuando se hundan todos los barcos de algun jugador")
+print("-Para salir el juego antes de que se hundan todos los barcos introduce la palabra 'exit' (sin comillas)\n")
+
+
 #Se crean los tableros del usuario y de la máquina
 tablero_usuario = clases.Tablero(variables.DIMENSION_FILA, variables.DIMENSION_COLUMNA)
 tablero_maquina = clases.Tablero(variables.DIMENSION_FILA, variables.DIMENSION_COLUMNA)
@@ -18,78 +31,90 @@ for barco in variables.lista_barcos:
         funciones.posicionar_aleatorio(tablero_usuario.tablero_barcos, tamano_barco, variables.DIMENSION_FILA-1, variables.DIMENSION_COLUMNA-1)
         funciones.posicionar_aleatorio(tablero_maquina.tablero_barcos, tamano_barco, variables.DIMENSION_FILA-1, variables.DIMENSION_COLUMNA-1)
 
-#Iniciamos el juego con un while que finalizará si el usuario indica "exit" o no hay barcos en algun tablero
-fin_juego = False
-usuario = True
+#Preguntamos al usuario la dificultad en la que quiere jugar
 
-while fin_juego == False:
+dificultad = input("Indica la dificultad del juego entre estas opciones: facil, media y dificil:").lower()
 
-    if usuario == True: #Si le toca al usuario
-        
-        tablero_a_disparar = tablero_maquina.tablero_barcos #Dispara el usuario
-        tablero_registro_disparo = tablero_usuario.tablero_registro_disparos #se guardan los disparos hechos por el usuario
+#Seleccionamos la cantidad de disparos de la máquina en funcion de la dificultad
+if dificultad == "facil" or dificultad == "media" or dificultad == "dificil":
 
-        continuar = True
-        while continuar == True:
+    if dificultad =="facil":
+        repeticion_maquina = 1
+    elif dificultad =="media":
+        repeticion_maquina = 2
+    elif dificultad =="dificil":
+        repeticion_maquina = 3
+
+    #Iniciamos el juego con un while que finalizará si el usuario indica "exit" o no hay barcos en algun tablero
+    fin_juego = False
+    usuario = True
+
+    while fin_juego == False:
+
+        if usuario == True: #Si le toca al usuario
+
+            print("Tu tablero de registro de disparos:")
+            print(tablero_usuario.tablero_registro_disparos,"\n")
+
+            print("Tu tablero de barcos:")
+            print(tablero_usuario.tablero_barcos,"\n")
+
+            tablero_a_disparar = tablero_maquina.tablero_barcos #Dispara el usuario
+            tablero_registro_disparo = tablero_usuario.tablero_registro_disparos #se guardan los disparos hechos por el usuario
+
+            continuar = True
+            while continuar == True:
+                
+                coordenadas_str = input("Tu turno. Introduce la coordenada de la fila y la coordenada de la columna separadas por coma:")
+                coordenadas_lista = coordenadas_str.split(",")
+
+                if coordenadas_lista[0] == "exit":
+                    print("Ha finalizado el juego")
+                    sys.exit()
+
+                continuar = funciones.disparo(tablero_a_disparar, tablero_registro_disparo, int(coordenadas_lista[0]), int(coordenadas_lista[1])) 
+
+                #print("Tu tablero de registro de disparos:")
+                #print(tablero_usuario.tablero_registro_disparos)
+
+                #print("Tu tablero de barcos:")
+                #print(tablero_usuario.tablero_barcos)
+                
+                fin_juego = funciones.comprobar_fin_juego(tablero_a_disparar)
             
-            coordenadas_str = input("Tu turno. Introduce la coordenada de la fila y la coordenada de la columna separadas por coma:")
-            coordenadas_lista = coordenadas_str.split(",")
-
-            if coordenadas_lista[0] == "exit":
-                print("Ha finalizado el juego")
-                sys.exit()
-
-            continuar = funciones.disparo(tablero_a_disparar, tablero_registro_disparo, int(coordenadas_lista[0]), int(coordenadas_lista[1])) 
-
-            print("Tablero registro disparo usuario")
-            print(tablero_usuario.tablero_registro_disparos)
-
-            print("Tablero disparos recibidos usuario")
-            print(tablero_usuario.tablero_barcos)
-
-            #print("Tablero registro disparo maquina")
-            #print(tablero_maquina.tablero_registro_disparos)
-
-            #print("Tablero disparos recibidos maquina")
-            #print(tablero_maquina.tablero_barcos,"\n")
+                if fin_juego == True: 
+                    print("Has ganado")
+                    sys.exit()
             
-            fin_juego = funciones.comprobar_fin_juego(tablero_a_disparar)
-        
-            if fin_juego == True: 
-                print("Ha ganado el usuario")
-                sys.exit()
-        
-        usuario = False
-               
+            usuario = False
 
-    else: #Si le toca a la máquina
-        tablero_a_disparar = tablero_usuario.tablero_barcos #Dispara la maquina
-        tablero_registro_disparo = tablero_maquina.tablero_registro_disparos #se guardan los disparos hechos por la maquina
+        elif usuario == False: #Si le toca a la máquina
 
-        continuar = True
-        while continuar == True:
+            tablero_a_disparar = tablero_usuario.tablero_barcos #Dispara la maquina
+            tablero_registro_disparo = tablero_maquina.tablero_registro_disparos #se guardan los disparos hechos por la maquina
             
-            coordenada_fila_maquina = np.random.randint(0,variables.DIMENSION_FILA)
-            coordenada_columna_maquina = np.random.randint(0,variables.DIMENSION_COLUMNA)
+            for disparo_maquina in range(repeticion_maquina):
 
-            continuar = funciones.disparo(tablero_a_disparar, tablero_registro_disparo, coordenada_fila_maquina, coordenada_columna_maquina)
-        
-            #print("Tablero registro disparo usuario")
-            #print(tablero_usuario.tablero_registro_disparos)
+                continuar = True
+                while continuar == True:
+                    
+                    coordenada_fila_maquina = np.random.randint(0,variables.DIMENSION_FILA)
+                    coordenada_columna_maquina = np.random.randint(0,variables.DIMENSION_COLUMNA)
 
-            #print("Tablero disparos recibidos usuario")
-            #print(tablero_usuario.tablero_barcos)
+                    continuar = funciones.disparo(tablero_a_disparar, tablero_registro_disparo, coordenada_fila_maquina, coordenada_columna_maquina)
 
-            print("Tablero registro disparo maquina")
-            print(tablero_maquina.tablero_registro_disparos)
+                    #print("Tablero registro disparo maquina")
+                    #print(tablero_maquina.tablero_registro_disparos)
 
-            print("Tablero disparos recibidos maquina")
-            print(tablero_maquina.tablero_barcos,"\n")
-            
-            fin_juego = funciones.comprobar_fin_juego(tablero_a_disparar)
+                    #print("Tablero disparos recibidos maquina")
+                    #print(tablero_maquina.tablero_barcos,"\n")
+                    
+                    fin_juego = funciones.comprobar_fin_juego(tablero_a_disparar)
 
-            if fin_juego == True: 
-                print("Ha ganado la máquina")
-                sys.exit()
+                    if fin_juego == True: 
+                        print("Ha ganado la máquina")
+                        sys.exit()
 
-        usuario = True
+            usuario = True
+else:
+    print("La dificultad elegida no es válida. Vuelve a empezar")
