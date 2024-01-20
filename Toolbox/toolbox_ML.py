798505d9f1a804ebf33ab09a788fcd873df15466
Rogelio Garcia <147382528+RogelioGarciaAlvarez@users.noreply.github.com>
2024-01-20 16:26:25 +0100

    funcion tipifica_variables incluida

--- Toolbox/toolbox_ML.py
+++ Toolbox/toolbox_ML.py
@@ -19,20 +19,54 @@ def describe_df():
     return "X"
 
 #Función tipifica_variables (encargado: Rogelio)
-def tipifica_variables():
+def tipifica_variables(df, umbral_categoria, umbral_continua):
+    
     """
-    Descripción breve de lo que hace la función.
+    Función que sugiere tipos para la tipificación de las variables (columnas) de un DataFrame.
 
     Argumentos:
-    param1 (tipo): Descripción de param1.
-    param2 (tipo): Descripción de param2.
+    df (DataFrame): DataFrame cuyas variables queremos tipificar
+    umbral_categoria (int): valor límite para clasificar una variable como categórica
+    umbral_numerica (float): valor límite para clasificar una variable como numérica continua
 
     Retorna:
-    tipo: Descripción de lo que retorna la función.
+    DataFrame: devuelve un DataFrame con tres columnas: "nombre_variable", "tipo_variable" y "tipo_sugerido".
     """
-    #Cuerpo de la función
+    #EXTRA: creamos una lista con el tipo de cada variable
+    lista_tipos = []
     
-    return "X"
+    #Creamos una lista con la tipificación sugerida para cada variable
+    lista_tipificacion = []
+
+    #Sugerimos la tipificación de la variable y la guardamos en la lista
+    for columna in df.columns:
+        
+        #EXTRA: Guardamos en la lista el tipo de cada variable
+        lista_tipos.append(df[columna].dtypes)
+        
+        #Calculamos la cardinalidad
+        cardinalidad = df[columna].nunique()
+        
+        #Clasificamos segun el valor de la cardinalidad
+        if cardinalidad == 2:
+            lista_tipificacion.append("Binaria")
+            
+        elif cardinalidad >= umbral_categoria:
+            
+            if cardinalidad >= umbral_continua:
+                lista_tipificacion.append("Numérica continua")
+                
+            else: 
+                lista_tipificacion.append("Numérica discreta")
+
+        else:
+            lista_tipificacion.append("Categórica")
+
+    
+    #Creamos el DataFrame con tantas filas como columnas tenga el DataFrame dado a la función
+    df_tipifica = pd.DataFrame({"nombre_variable": df.columns.tolist(), "tipo_variable":lista_tipos, "tipo_sugerido":lista_tipificacion})
+
+    return df_tipifica 
 
 #Función get_features_num_regression (encargado: Marion)
 def get_features_num_regression():
