49c8a5074f1ba9f8094e8523a1eff8fe6e15ca0a
Rogelio Garcia <147382528+RogelioGarciaAlvarez@users.noreply.github.com>
2024-01-20 14:14:28 +0100

    avances 20/01

---
+++ Toolbox/toolbox_ML.py
@@ -0,0 +1,100 @@
+#Importamos las librerías
+import pandas as pd
+
+
+#Función describe_df (encargado: Marion)
+def describe_df():
+    """
+    Descripción breve de lo que hace la función.
+
+    Argumentos:
+    param1 (tipo): Descripción de param1.
+    param2 (tipo): Descripción de param2.
+
+    Retorna:
+    tipo: Descripción de lo que retorna la función.
+    """
+    #Cuerpo de la función
+    
+    return "X"
+
+#Función tipifica_variables (encargado: Rogelio)
+def tipifica_variables():
+    """
+    Descripción breve de lo que hace la función.
+
+    Argumentos:
+    param1 (tipo): Descripción de param1.
+    param2 (tipo): Descripción de param2.
+
+    Retorna:
+    tipo: Descripción de lo que retorna la función.
+    """
+    #Cuerpo de la función
+    
+    return "X"
+
+#Función get_features_num_regression (encargado: Marion)
+def get_features_num_regression():
+    """
+    Descripción breve de lo que hace la función.
+
+    Argumentos:
+    param1 (tipo): Descripción de param1.
+    param2 (tipo): Descripción de param2.
+
+    Retorna:
+    tipo: Descripción de lo que retorna la función.
+    """
+    #Cuerpo de la función
+    
+    return "X"
+
+#Función plot_features_num_regression (encargado: Rogelio)
+def plot_features_num_regression():
+    """
+    Descripción breve de lo que hace la función.
+
+    Argumentos:
+    param1 (tipo): Descripción de param1.
+    param2 (tipo): Descripción de param2.
+
+    Retorna:
+    tipo: Descripción de lo que retorna la función.
+    """
+    #Cuerpo de la función
+    
+    return "X"
+
+
+#Función get_features_cat_regression (encargado: Rogelio)
+def get_features_cat_regression():
+    """
+    Descripción breve de lo que hace la función.
+
+    Argumentos:
+    param1 (tipo): Descripción de param1.
+    param2 (tipo): Descripción de param2.
+
+    Retorna:
+    tipo: Descripción de lo que retorna la función.
+    """
+    #Cuerpo de la función
+    
+    return "X"
+
+#Función plot_features_cat_regression (encargado: Numa)
+def plot_features_cat_regression():
+    """
+    Descripción breve de lo que hace la función.
+
+    Argumentos:
+    param1 (tipo): Descripción de param1.
+    param2 (tipo): Descripción de param2.
+
+    Retorna:
+    tipo: Descripción de lo que retorna la función.
+    """
+    #Cuerpo de la función
+    
+    return "X"
